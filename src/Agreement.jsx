1a39c0c42c356af8f00459f5b003d9a68a86a011
maryyygb <marygbasilio8@gmail.com>
2026-05-12 23:26:18 +0800

    finished the Agreement.jsx and started the Reminder.jsx

---
+++ src/Agreement.jsx
@@ -0,0 +1,100 @@
+/* eslint-disable no-unused-vars */
+import {useEffect, useState} from 'react'
+import { VscHeart, VscHeartFilled } from "react-icons/vsc";
+
+
+function Agreement() {
+    
+    let name = "JEFF";
+    name = name.toLowerCase();
+
+    const [access, setAccess] = useState(false);
+
+    const [agree, isAgree] = useState({
+                                term0: false, 
+                                term1: false, 
+                                term2: false});
+
+    // FUNCTIONS
+    const conditions = ["I will read each message carefully and completely, knowing it was written with true sincerity and courage.",
+                        "I will respond with absolute kindness and honesty, no matter what my final answer may be.", 
+                        "I understand completely that this is a deeply private space meant strictly between the two of us."
+    ];
+
+    const handleConditions = (i) => {
+
+        const key = `term${i}`
+        isAgree(p => ({...p, [key]: !p[key]}))
+    }
+
+    
+    
+    useEffect(() => {
+
+        const allChecked = Object.values(agree).every(val => val === true);
+
+        setAccess(allChecked)
+
+                console.log("Updated Agreement State: ", agree);
+
+            }, [agree])
+
+    const just = () => {
+        console.log("This one says it match the name")
+    }
+
+  return (
+    <div className="container ag_con">
+        <div className="orbs">
+            <div className="orb1"></div>
+            <div className="orb2"></div>
+            <div className="orb3"></div>
+            <div className="orb4"></div>
+
+            <div className="orb5"></div>
+            <div className="orb6"></div>
+        </div>
+        <div className="card_ag">
+            <div className="deco_ag">
+                <p>✦</p>
+                <p>✦</p>
+            </div>
+            <h1 className="agreement_title">a few things first,</h1> 
+            <p className="userName">{name}</p>
+            <p className="ag_reminder_one">what you're about to read is real.</p>
+            <p className="ag_reminder_two">please agree before continuing.</p>
+
+            <ul className="terms">
+                {conditions.map((condition, i) => {
+                    const isTermAgreed = agree[`term${i}`]
+
+                    return (
+                       <li 
+                       className={`term ${isTermAgreed ? 'agreed' : 'not_agreed'}`}
+ 
+                       onClick={() => handleConditions(i)} 
+                       key={i}>
+                    <p className="checkbox">
+                        {isTermAgreed ? (<VscHeartFilled className='heart_i'/>) : (<VscHeart className='heart_i'/>) }
+                    
+                    </p>
+                    <p className='conditions'>{condition}</p>
+                </li> 
+                    )})
+                }
+                
+            </ul>
+
+            <button
+                    className={`verify_btn_ag ${access ? 'verified' : 'not_verified'}`}
+                    disabled={!access}
+                    onClick={just}
+                >
+                    {access ? "i agree — show me →" : "you must agree to continue"}
+                </button>
+        </div>
+    </div>
+  )
+}
+
+export default Agreement
---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+/* eslint-disable no-unused-vars */
+import { useState } from 'react';
+import './App.css'
+
+import Home from "./Home"
+import Agreement from './Agreement';
+import Reminder from './Reminder';
+
+function App() {
+
+  return (
+    <>
+        {/* <Home/> */}
+        {/* <Agreement/> */}
+        <Reminder/>
+    </>
+  )
+}
+
+export default App
