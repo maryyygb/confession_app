d9c1d5b3307451da49de0b2a99cf953ffce33431
maryyygb <marygbasilio8@gmail.com>
2026-05-16 22:56:45 +0800

    ChatRoom: Added style to what the chat box looks like. Need to do the style for each messages.

--- justforyou/src/ChatRoom.jsx
+++ justforyou/src/ChatRoom.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-function ChatRoom() {
+const ChatRoom = () => {
+
+  const [message, setMessage] = useState(null);
+
   return (
-    <div>ChatRoom</div>
+    <div className="chatroom_box">
+      <div className="orbs rem_obs croom_orbs">
+            <div className="orb1"></div>
+            <div className="orb2"></div>
+            <div className="orb3"></div>
+            <div className="orb4"></div>
+
+            <div className="orb5"></div>
+            <div className="orb6"></div>
+        </div>
+        <nav>
+          <ul className="croom_nav">
+            <li className="nav-items">✦</li>
+            <li className="nav-items">
+              <p className="me">someone you used to call EmEm</p>
+              <p className="my_status">• confused girl right here</p>
+            </li>
+          </ul>
+        </nav>
+
+        <footer>
+          <input className="chat_input" placeholder="type your message here" type="text" value={message} onChange={(e) => setMessage(e.target.value)}/>
+          <button className={message ? "chat_send" : "chat_send_disabled"} type="submit">↑</button>
+        </footer>
+    </div>
   )
 }
 
